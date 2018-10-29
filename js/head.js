ea3433e15d6bb10bfa16e7a3663a4206af254a85
高志强 <gaozq@beagledata.com>
2018-10-29 15:49:22 +0800

    Site updated: 2018-10-29 15:49:21

--- js/head.js
+++ js/head.js
@@ -26,18 +26,18 @@ $(function () {
   if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) { //移动端
       return false;
   }
-  $('body').prepend(`<div id="landlord">
-                        <div class="message" style="opacity:0"></div>
-                        <canvas id="live2d" width="280" height="250" class="live2d"></canvas>
-                        <div class="hide-button">隐藏</div>
-                    </div>`);
-  $('head').append(`<link rel="stylesheet" href="https://blog.xiqing520.com/css/live2d.css" />`);
+  // $('body').prepend(`<div id="landlord">
+  //                       <div class="message" style="opacity:0"></div>
+  //                       <canvas id="live2d" width="280" height="250" class="live2d"></canvas>
+  //                       <div class="hide-button">隐藏</div>
+  //                   </div>`);
+  // $('head').append(`<link rel="stylesheet" href="https://blog.xiqing520.com/css/live2d.css" />`);
 
-  $('body').append(`<script type="text/javascript" src="https://blog.xiqing520.com/js/live.min.js"></script>`);
-  $('body').append(`<script type="text/javascript" src="https://blog.xiqing520.com/js/message.js"></script>`);
-    let lastId=Math.ceil(Math.random()*20);
-    let src='https://api.fghrsh.net/live2d/get/?id=6-'+lastId;
-    // let src="../../static/live2d/live2d-widget-model-nipsilon/assets/nipsilon.model.json"
-    loadlive2d("live2d", src);
+  // $('body').append(`<script type="text/javascript" src="https://blog.xiqing520.com/js/live.min.js"></script>`);
+  // $('body').append(`<script type="text/javascript" src="https://blog.xiqing520.com/js/message.js"></script>`);
+  //   let lastId=Math.ceil(Math.random()*20);
+  //   let src='https://api.fghrsh.net/live2d/get/?id=6-'+lastId;
+  //   // let src="../../static/live2d/live2d-widget-model-nipsilon/assets/nipsilon.model.json"
+  //   loadlive2d("live2d", src);
 
 })
